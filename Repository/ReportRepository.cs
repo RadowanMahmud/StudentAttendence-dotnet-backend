4ed1852eb70cc200deb55a4ccada9435527423cf
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-07-27 19:35:32 +0600

    This is the repo less version of our project

---
+++ Repository/ReportRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentTeendanceBackend.Data;
+
+namespace StudentTeendanceBackend.Repository
+{
+    public class ReportRepository
+    {
+        private readonly StudentTeendanceBackendContext _context;
+
+        public ReportRepository(StudentTeendanceBackendContext context)
+        {
+            _context = context;
+        }
+
+    }
+
+    public class MyRecord
+    {
+        public object record { get; set; }
+        public object attendence { get; set; }
+        public object admin { get; set; }
+    }
+}
