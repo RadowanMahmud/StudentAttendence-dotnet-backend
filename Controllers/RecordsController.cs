cbca43bbc003b2c52eab1f62170a5f9b3a50eb81
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-07-27 11:06:01 +0600

    weekly report added

--- Controllers/RecordsController.cs
+++ Controllers/RecordsController.cs
@@ -160,8 +160,39 @@ namespace StudentTeendanceBackend.Controllers
             return Ok(listofMyrecords);
         }
 
+        [HttpGet("reports/weekly/{studentId}")]
+        public async Task<ActionResult<IEnumerable<Record>>> GetMonthlyWeeklyByStudentId(string month, int studentId)
+        {
+            DayOfWeek currentDay = DateTime.Now.DayOfWeek;
+            int daysTillCurrentDay = currentDay - DayOfWeek.Sunday;
+            int daysFromCurrentDay = DayOfWeek.Saturday - currentDay;
+            DateTime currentWeekStartDate = DateTime.Now.AddDays(-daysTillCurrentDay);
+            DateTime currentWeekEndDate = DateTime.Now.AddDays(daysFromCurrentDay);
+
+
+            var records = await _context.Record
+                .Where(a => a.GivenTime.Date >= currentWeekStartDate.Date && a.GivenTime.Date <= currentWeekEndDate.Date && a.StudentId == studentId).ToListAsync();
+
+            var listofMyrecords = new List<MyRecord>();
+
+            foreach (var record in records)
+            {
+                var temp = new MyRecord();
+                temp.record = record;
+                var attendence = _context.Attendance.Where(p => p.Id == record.attendancesId).FirstOrDefault();
+                temp.attendence = attendence;
+                temp.admin = _context.Admin.Where(p => p.Id == attendence.AdminId).FirstOrDefault();
+
+                listofMyrecords.Add(temp);
+
+            }
+
+            return Ok(listofMyrecords);
+        }
+
 
     }
+
 
     public class MyRecord
     {
@@ -169,4 +200,7 @@ namespace StudentTeendanceBackend.Controllers
         public object attendence { get; set; }
         public object admin { get; set; }
     }
+
+
 }
+
